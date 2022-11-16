d56cf6f9076ea8cfb5ab6bf9f01501e6619d176e
Gonpassa <Gonzalopassa@hotmail.com>
2022-11-16 16:27:43 -0500

    issues with request, created more templates

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-import os
+import json
 import datetime
+from cs50 import SQL
 from flask import Flask
-from flask import request, render_template, redirect, session
+from flask import request, render_template, redirect, session, flash
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-
+#Configure application
 app = Flask(__name__)
+
+#Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+#Configure session to use filesystem instead of signed cookies
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Use SQLite
+db = SQL("sqlite:///goals.db")
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 
 #HOME PAGE
@@ -29,24 +50,49 @@ def login():
             error = 'Invalid password'
             return render_template('login.html', error=error)
 
+        #Query database for username
         username = request.form.get('username')
+        password = request.form.get('password')
+    
+        rows = db.execute("SELECT * FROM users WHERE username = ?", username.lower())
+        
+        #Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]['hash'], password):
+            error = 'Invalid username or password'
+            return render_template('login.html', error=error)
+        
+        # Remember user that logged in
+        session['user_id'] = rows[0]['id']
 
-        return do_the_login()
+        
+        return render_template('index.html')
     return render_template('login.html')
 
 @app.route('/register', methods = ["GET", "POST"])
 def register():
     if request.method == "POST":
-        username = request.method.get('username')
-        password = request.method.get('password')
-        confirm_password = request.method.get('confirm')
-        
+        username = request.form.get("username")
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm')
+
         if not username or not password or not confirm_password:
             error = "Please fill in all fields"
-            return render_template('register.html', error=error)
+            return render_template('register.html', error=json.dumps(error))
         if password != confirm_password:
             error = "Passwords don't match"
-            return render_template('register.html', error=error)
+            return render_template('register.html', error=json.dumps(error))
+
         
+
+        usernames = db.execute("SELECT username FROM user")
+        username = username.lower()
+        if username in usernames:
+            error = 'Username taken'
+            return render_template('register.html', error=json.dumps(error))
+
+        hash = generate_password_hash(password)
+
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
+        return redirect('login.html')
 
     return render_template('register.html')
